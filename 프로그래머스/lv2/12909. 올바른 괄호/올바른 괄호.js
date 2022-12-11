d319e61ae3f6c89b9f9084e16f76d05068caa349
ho04029 <96459913+ho04029@users.noreply.github.com>
2022-12-11 22:22:03 +0900

    [level 2] Title: 올바른 괄호, Time: 2.34 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,12 @@
+function solution(s){
+    let cnt = 0
+    
+    for(let i = 0; i < s.length ; i+=1){
+        s[i] === "(" ? cnt++ : cnt--;
+        if(cnt < 0){
+            return false
+        }
+    }
+
+    return cnt === 0 ? true : false ;
+}
